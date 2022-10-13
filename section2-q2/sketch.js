061084294b9844d00463a74016846f1d0df33e0d
2061231h <2061231h@stu.kobe-u.ac.jp>
2022-10-14 00:50:28 +0900

    Revert "Changed section2-q2"
    This reverts commit 830d129184d12bb139985f70729ddefb32dd9bf6.

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,21 +3,9 @@ function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
-  for(let i = 0; i < 8; i++)
+  for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    if((i+j) % 2 == 1){
-    fill(128,128,128)
-  rect(size * i,size * j,size,size)};
-  if ((i+j) % 2 == 1){
-    if (j<3){
-  fill(255,0,0)}
-  else if(j>=3 && j<5) {
-    fill(128,128,128)}
-    else {
-fill(0,0,0)
     }
-  ellipse( size * (i + 0.5) , size *(j+0.5), size * 0.9);
-}
-}
+  }
 }
